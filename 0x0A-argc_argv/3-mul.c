ffa5140f94cfb0139ea5575a731d3a61767be268
Mohammed <simoaddame17@gmail.com>
2024-01-12 06:51:22 -0800

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,12 +9,19 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	int num1, num2;
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
