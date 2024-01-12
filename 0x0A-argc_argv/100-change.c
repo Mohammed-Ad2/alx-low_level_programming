83d86ac40d4c36a32c4d9a4fde3a812f52f0e4e2
Mohammed <simoaddame17@gmail.com>
2024-01-12 08:00:38 -0800

    Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,79 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- * count_coins - Count the minimum number of coins
- *		 needed to make change.
- * @cents: The amount in cents for which to calculate
- *	   the minimum coins.
- *
- * Return: The minimum number of coins needed.
- */
-int count_coins(int cents)
-{
-	int coins = 0;
-
-	while (cents > 0)
-	{
-
-		if (cents >= 25)
-		{
-			cents -= 25;
-			coins++;
-		}
-		else if (cents >= 10)
-		{
-			cents -= 10;
-			coins++;
-		}
-		else if (cents >= 5)
-		{
-			cents -= 5;
-			coins++;
-		}
-		else if (cents >= 2)
-		{
-			cents -= 2;
-			coins++;
-		}
-		else if (cents >= 1)
-		{
-			cents -= 1;
-			coins++;
-		}
-	}
-
-	return (coins);
-}
-
 /**
  * main - Main Entry
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
- *
+ * @argc: Argument count
+ * @argv: Argument vector
  * Return: Always 0 (success)
- **/
+**/
 int main(int argc, char *argv[])
 {
+	int cents = atoi(*(argv + 1)), coins = 0;
+
 	if (argc == 2)
 	{
-		int cents = atoi(argv[1]);
-
-		if (cents < 0)
+		while (cents > 0)
 		{
-			printf("0\n");
-			return (0);
+			if (cents % 25 < cents)
+			{
+				cents -= 25;
+				coins++;
+			}
+			else if (cents % 10 < cents)
+			{
+				cents -= 10;
+				coins++;
+			}
+			else if	(cents % 5 < cents)
+			{
+				cents -= 5;
+				coins++;
+			}
+			else if	(cents % 2 < cents)
+			{
+				cents -= 2;
+				coins++;
+			}
+			else if (cents % 1 < cents)
+			{
+				cents -= 1;
+				coins++;
+			}
 		}
-
-		int coins = count_coins(cents);
-
-		printf("%d\n", coins);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
+	printf("%d\n", coins);
 	return (0);
 }
