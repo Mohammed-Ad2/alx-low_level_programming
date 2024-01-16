95f0935a2043b05b52be9be7a1e1951ecda17fa8
Mohammed <simoaddame17@gmail.com>
2024-01-16 06:46:45 -0800

    Update Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,46 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the times table of the input,
- *		       starting with 0.
- *
- * @n: The value of the times table to be printed.
-*/
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The times table to print (up to 15).
+ */
 void print_times_table(int n)
 {
 	if (n > 15 || n < 0)
-	{
-	}
-	else
-	{
-		int i, j, p = 0;
+		return;
 
-		for (i = 0; i <= n; i++)
+	int i, j, p;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			p = i * j;
+
+			if (j != 0)
 			{
-				p = i * j;
-				if (p > 0 && p <= 9 || j == 0)
-					_putchar(p + '0');
-				if (p > 9 && p < 100)
-				{
-					_putchar(p / 10 + '0');
-					_putchar(p % 10 + '0');
-				}
-				if (p >= 100)
-				{
-					_putchar(p / 100 + '0');
-					_putchar((p / 10 % 10) + '0');
-					_putchar(p % 10 + '0');
-				}
-				if (j != n)
-				{
-					_putchar(',');
+				_putchar(',');
+				_putchar(' ');
+
+				if (p <= 9)
 					_putchar(' ');
-				}
 			}
-			_putchar('\n');
+
+			if (p <= 9)
+				_putchar(p + '0');
+			else if (p <= 99)
+			{
+				_putchar((p / 10) + '0');
+				_putchar((p % 10) + '0');
+			}
+			else
+			{
+				_putchar((p / 100) + '0');
+				_putchar(((p / 10) % 10) + '0');
+				_putchar((p % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
+
