557c56ee47e0e560d35202ee49fc193b768a1bd9
Mohammed <simoaddame17@gmail.com>
2024-01-18 05:47:52 -0800

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -13,7 +12,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	int i, j, len1, len2;
+	unsigned int i, len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
