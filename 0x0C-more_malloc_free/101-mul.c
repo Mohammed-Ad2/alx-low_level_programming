041a3c57bdd06e7a4030ec56bee2805ae12c591b
Mohammed <simoaddame17@gmail.com>
2024-01-18 10:44:46 -0800

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 
 /**
+ * is_digit - Checks if a string consists of digits
+ * @str: The string to check
+ *
+ * Return: 1 if all characters are digits, 0 otherwise
+ */
+int is_digit(char *str)
+{
+	while (*str)
+	{
+	if (*str < '0' || *str > '9')
+		return (0);
+		str++;
+	}
+	return (1);
+}
+
+/**
+ * multiply - Multiplies two numbers
+ * @num1: The first number
+ * @num2: The second number
+ *
+ * Return: The result of the multiplication
+ */
+int multiply(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+/**
  * main - Multiplies two positive numbers
  * @argc: The argument count
  * @argv: An array of strings containing the command-line arguments
@@ -10,10 +39,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int *ptr;
-	int num1, num2;
+	int num1, num2, result;
 
-	if (argc != 3)
+	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
 	{
 		printf("Error\n");
 		exit(98);
@@ -22,23 +50,8 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 
-	if (!(num1 >= 0 && num1 <= 9) || !(num2 >= 0 && num2 <= 9))
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	ptr = malloc(sizeof(int));
-	if (ptr == NULL)
-	{
-		printf("Memory allocation error\n");
-		exit(1);
-	}
-
-	*ptr = num1 * num2;
-	printf("%d\n", *ptr);
-
-	free(ptr);
+	result = multiply(num1, num2);
+	printf("%d\n", result);
 
 	return (0);
 }
