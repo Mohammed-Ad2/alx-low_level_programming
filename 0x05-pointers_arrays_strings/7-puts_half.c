e354d2a66d11922c816d36145e9d74ac17234976
Mohammed <simoaddame17@gmail.com>
2024-01-19 08:49:17 -0800

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ *	       if len is odd, n = (len - 1) / 2
+ * @str: input
+ */
+void puts_half(char *str)
+{
+	int i, len = 0, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+		for (i = n; i < len; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		n = ((len - 1) / 2);
+		for (i = n; i < len; i++)
+			_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
