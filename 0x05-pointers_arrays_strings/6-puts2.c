346f9ab90c43f83e12799994d2062b403da03971
Mohammed <simoaddame17@gmail.com>
2024-01-19 08:41:33 -0800

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - Print only one character out of two
+ *	   starting with the first one
+ * @str: input
+ */
+void puts2(char *str)
+{
+	int i, len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	len--;
+
+	for (i = 0; i <= len; i++)
+		if (i % 2 == 0)
+			putchar(str[i]);
+
+	putchar('\n');
+}
+
