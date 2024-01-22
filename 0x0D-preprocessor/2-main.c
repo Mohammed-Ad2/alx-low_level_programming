7cd0f3b99f74fe66c87470e2370a77e73dc20c7c
Mohammed <simoaddame17@gmail.com>
2024-01-22 05:53:51 -0800

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,9 +3,11 @@
 /**
  * main - Print the name of the current source file.
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("File :%s/n", __FILE__);
+
+	return (0);
 }
