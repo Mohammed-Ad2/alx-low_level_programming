3c64557489b901e3e09422ff9df3f3b829325e7b
Mohammed <simoaddame17@gmail.com>
2024-01-25 12:50:14 -0800

    Header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,20 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/**
+ * struct formatter - A new struct type defining a printer.
+ * @spec: A symbol representing a data type.
+ * @print: A function pointer that prints a data
+ *         type corresponding to symbol.
+ */
+typedef struct formatter
+{
+	char spec;
+	void (*print)(va_list *);
+} fmt;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
