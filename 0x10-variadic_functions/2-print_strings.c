f6ab67f9b3132c31ff60564f9eafd76bf2d08907
Mohammed <simoaddame17@gmail.com>
2024-01-25 08:59:38 -0800

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", current_str);
 
-		if (separator != NULL && i != (n - 1))
+		if (separator && i != (n - 1))
 			printf("%s", separator);
 	}
 
