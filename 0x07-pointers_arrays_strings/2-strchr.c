0193ea3a9b220fc2eedd39f838d2a401cca74164
Mohammed <simoaddame17@gmail.com>
2024-02-03 14:45:31 -0800

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			return (&s[i]);
 
 	if (c == '\0')
-		return (s);
+		return (&s[i]);
 
 	return (NULL);
 }
