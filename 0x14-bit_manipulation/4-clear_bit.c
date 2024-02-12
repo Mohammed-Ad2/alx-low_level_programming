393b8aa6dcfd03e5ad7261ed662086dfa6ca8b5c
Mohammed <simoaddame17@gmail.com>
2024-02-12 04:23:07 -0800

    Task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,10 +9,13 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask;
+
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	*n &= ~(1UL << index);
+	mask = ~(1UL << index);
+	*n &= mask;
 
 	return (1);
 }
