95d57fc2cd79c8538775a460b8f7cd366355901f
Mohammed <simoaddame17@gmail.com>
2024-02-12 13:30:59 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,17 +9,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, rwr;
+	int fd, rwr, len;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 		text_content = "";
+
+	for (len = 0; text_content[len]; len++)
+		;
 
 	rwr = write(fd, text_content, strlen(text_content));
 	if (rwr == -1)
