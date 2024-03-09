8147f2e26b078670a2685ade34c7ccc734711254
Adam <simoaddame17@gmail.com>
2024-03-09 19:10:30 +0100

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, a = 0;
+	int i = 0;
 
-	for (i = 0; s1[i] != '\0' && a == 0; i++)
-		a = s1[i] - s2[i];
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
 
-	return (a);
+	return (0);
 }
