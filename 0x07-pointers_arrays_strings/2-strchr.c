a79dbea7b113a802c8a6d62912f0c31f9a9c2be6
Adam <simoaddame17@gmail.com>
2024-03-30 22:36:30 +0100

    update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,8 +5,8 @@
  * @s: The string to be searched.
  * @c: The character to be located.
  *
- * Return: Pointer to the first occurrence of the character c in the string s
- *	   NULL if the character is not found.
+ * Return: A pointer to the first occurrence of the character c
+ *         in the string s, or NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
 {
@@ -17,5 +17,8 @@ char *_strchr(char *s, char c)
 		s++;
 	}
 
-	return (0);
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
 }
