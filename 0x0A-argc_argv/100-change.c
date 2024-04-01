7ccbb14db178680a9ab1e05fc0f9cdcd8528fda2
Adam <simoaddame17@gmail.com>
2024-04-01 22:22:36 +0200

    Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -16,34 +16,25 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	cents = atoi(*(argv + 1));
+	cents = atoi(argv[1]);
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
 	while (cents > 0)
 	{
-		if (cents % 25 < cents)
-		{
-			cents -= 25;
-			coins++;
-		}
-		else if (cents % 10 < cents)
-		{
-			cents -= 10;
-			coins++;
-		}
-		else if	(cents % 5 < cents)
-		{
-			cents -= 5;
-			coins++;
-		}
-		else if	(cents % 2 < cents)
-		{
-			cents -= 2;
-			coins++;
-		}
-		else if (cents % 1 < cents)
-		{
-			cents -= 1;
-			coins++;
-		}
+		if (cents >= 25)
+			cents -= 25, coins++;
+		else if (cents >= 10)
+			cents -= 10, coins++;
+		else if (cents >= 5)
+			cents -= 5, coins++;
+		else if (cents >= 2)
+			cents -= 2, coins++;
+		else
+			cents -= 1, coins++;
 	}
 	printf("%d\n", coins);
 	return (0);
